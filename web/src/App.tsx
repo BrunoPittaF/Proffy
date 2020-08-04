2e95c1f3bd7d6d041ce31210f196ac3f9c6ba084
BrunoPittaF <caiobrunopittaf@gmail.com>
2020-08-04 16:33:59 -0300

    home page pronta

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/global.scss';
+import Landing from './pages/landing';
+
+function App() {
+  return (
+    <div className="App">
+      <Landing/>
+    </div>
+  );
+}
+
+export default App;
